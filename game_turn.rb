f0cbaa403d911ac1b1c6fb52c1c60b1108321d03
Jason Williams <jasonmw_nf@hotmail.com>
2015-04-22 11:40:39 -0230

    added random treasure finding

--- game_turn.rb
+++ game_turn.rb
@@ -1,5 +1,6 @@
 require_relative 'die'
 require_relative 'player'
+require_relative 'treasure_trove'
 
 module GameTurn
   def self.take_turn(player)
@@ -14,5 +15,8 @@ module GameTurn
     else
       puts "#{player.name} was skipped."
     end
+
+    treasure = TreasureTrove.random
+    puts "#{player.name} found a #{treasure.name} worth #{treasure.points} points."
   end
 end
